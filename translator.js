840e9c4ed482db1d17eadfdd58e0a55e22e9edc2
Alex Swan <alex@bold-it.com>
2016-11-13 15:00:14 -0600

    start translating pig latin

--- translator.js
+++ translator.js
@@ -1,15 +1,38 @@
 'use strict';
+var CMUDict = require('cmudict').CMUDict;
+var cmudict = new CMUDict();
 
 function Translator() {
-    
+    // var phoneme_str = cmudict.get('prosaic');
 }
 
 Translator.prototype.translate = function(phrase, language, cb){
     var result = {
         "type":"SSML",
-        "speech":"<speech>" + phrase + "</speech>"
+        "speech":"<speak>" + translatePigLatin(phrase) + "</speak>"
     }
     cb(result);
 };
+
+function translatePigLatin(phrase) {
+    var result = "";
+    // Split the phrase into words
+    var phrase_array = phrase.split();
+    for (var i = 0; i < phrase_array.length; i++) {
+        // Split each word into a phoneme string
+        var word = phrase_array[i];
+        var phoneme_str = cmudict.get(word);
+        var phoneme_array = phoneme_str.split();
+        
+        // Move the first phoneme to the end, add EY1
+        var first = phoneme_array.shift();
+        phoneme_array.push(first);
+        phoneme_array.push("eɪ");
+        result += "<phoneme alphabet='ipa' ph='" + phoneme_array.join() + "'>" + word + "</phoneme>"
+    }
+
+    // Turn the CMU Dict symbols to IPA
+    return result;
+}
 
 module.exports = Translator; 
